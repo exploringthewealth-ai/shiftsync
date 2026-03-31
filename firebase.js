2efa59dc09d8789af4da4c5979aa6264bb6549f3
exploringthewealth-ai <exploringthewealth@gmail.com>
2026-03-31 16:25:40 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_YOUR_APP_ID_HERE",
+  apiKey: "AIzaSyBo2F8ZpcJK-DoFZdpGe26tzsUTlyTbiiY",
+  authDomain: "shiftsync-7e5e8.firebaseapp.com",
+  projectId: "shiftsync-7e5e8",
+  storageBucket: "shiftsync-7e5e8.firebasestorage.app",
+  messagingSenderId: "1046433784725",
+  appId: "1:1046433784725:web:ba0404a634629308a00c9c",
 };
 
 const app = initializeApp(firebaseConfig);
